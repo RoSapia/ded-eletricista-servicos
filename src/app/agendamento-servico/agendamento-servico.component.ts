cf9bdd955f95af21fab61891022b225b52238b90
RoSapia <robertavsapia@gmail.com>
2024-09-28 17:50:20 -0300

    criacao backend

--- src/app/agendamento-servico/agendamento-servico.component.ts
+++ src/app/agendamento-servico/agendamento-servico.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClienteService } from '../cliente.service';
 @Component({
   selector: 'app-agendamento-servico',
   templateUrl: './agendamento-servico.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendamentoServicoComponent implements OnInit {
 
-  constructor() { }
+  cliente = {
+    nome: '',
+    telefone: '',
+    email: '',
+    endereco: '',
+    cep: '',
+    complemento: '',
+    ponto_referencia: ''
+  };
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
   }
-
+  
+  onSubmit(): void {
+    this.clienteService.adicionarCliente(this.cliente).subscribe(response => {
+      console.log('Cliente adicionado:', response);
+    });
+  }
 }
